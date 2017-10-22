18be6845fb7d955f7d626197a8744a50d6831618
Adriano Vega Llobell <adrianovega@hotmail.com>
2017-10-22 18:39:52 +0200

    Integrated the kanbanResume with the KanbanActivitiesService

--- src/app/modules/home/src/components/kanbanresume/kanbanresume.component.js
+++ src/app/modules/home/src/components/kanbanresume/kanbanresume.component.js
@@ -1,11 +1,44 @@
 import {Component} from '@angular/core';
+import {NotificationsService} from '@tune-up/core';
+import {KanbanActivitiesService} from './services'
+
 import html from './kanbanresume.component.html';
-// import './kanbanresume.component.css';
+import './kanbanresume.component.css';
 
 @Component({
   selector: 'tn-kanban-resume',
-  template: html
+  template: html,
+  providers: [KanbanActivitiesService]
 })
 export class KanbanResumeComponent {
-  Activities = [];
+  kanbanActivities = [];
+
+  constructor(
+    activitiesService : KanbanActivitiesService,
+    notificationsService : NotificationsService
+  ) {
+    this._activitiesService = activitiesService;
+    this._notificationsService = notificationsService;
+    this._getKanbanActivities();
+  }
+
+  _getKanbanActivities() {
+    this._activitiesService.get().subscribe(
+      data => {
+        if(!data.Exito) {
+          this._notificationsService.error(
+            'No se pudieron obtener las actividades del Kanban Resumido',
+            data.Mensaje
+          );
+          return;
+        }
+        this.kanbanActivities = data.Resultado;
+      }, 
+      error =>
+        this._notificationsService.error(
+          'No se pudieron obtener las actividades del Kanban Resumido',
+          error
+      )
+    );
+  };
 }

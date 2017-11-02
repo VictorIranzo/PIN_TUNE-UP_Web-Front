23f372b87f2c726e18337c7389e68293154c6d8e
Adriano Vega <adrianovega@hotmail.com>
2017-11-02 18:27:17 +0100

    initial component structure

---
+++ src/app/modules/ut/src/nuevaut/nuevaut.component.js
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import html from './nuevaut.component.html';
+import './nuevaut.component.css';
+
+@Component({
+  selector: 'tn-nuevaut',
+  template: html,
+})
+export class NuevaUT {
+  constructor() {
+  }
+}

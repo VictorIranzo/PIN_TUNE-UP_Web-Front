d1ba8b44b535da5a00155845f70931f9d5865a52
Adriano Vega Llobell <adrianovega@hotmail.com>
2017-11-08 18:26:48 +0100

    Integrated CreateUT and GetProduto services in the NuevaUT component

--- src/app/modules/ut/src/nuevaut/nuevaut.component.js
+++ src/app/modules/ut/src/nuevaut/nuevaut.component.js
@@ -1,12 +1,27 @@
 import {Component} from '@angular/core';
+import {NotificationsService} from '@tune-up/core';
+import {CreateUTService, GetProductosService} from './services';
 import html from './nuevaut.component.html';
 import './nuevaut.component.css';
 
 @Component({
-  selector: 'tn-nuevaut',
+  selector: 'tn-ut-nuevaut',
   template: html,
+  providers: [CreateUTService, GetProductosService],
 })
 export class NuevaUTComponent {
+  constructor(
+    createUTService: CreateUTService,
+    getProductosService : GetProductosService,
+    notificationsService: NotificationsService,
+  ) {
+    this._createUTService = createUTService;
+    this._getProductosService = getProductosService;
+    this._notificationService = notificationsService;
+
+    _getProductos();
+  }
+
   ut = {
     Nombre,
     IdVersion,
@@ -15,6 +30,35 @@ export class NuevaUTComponent {
     IdTipoUT,
     IdProyecto,
  }
-  constructor() {
+
+  productos = []
+  _getProductos() {
+    this._getProductosService.get().subscribe(
+      (data) => {
+        this.productos = data.Resultado;
+      },
+      (error) => {
+        this._notificationService.error(
+          'No se han podido obtener los productos',
+          error
+        );
+      }
+    );
+  }
+
+  crearUT() {
+    idUT = 0;
+    this.createUTService.put(this.ut).subscribe(
+      (data) => {
+         this.idUT = data.Resultado;
+      },
+      (error) => {
+        this._notificationService.error(
+          'No se pudo crear la UT especficada',
+          error
+        );
+      }
+    );
+    return idUT;
   }
 }

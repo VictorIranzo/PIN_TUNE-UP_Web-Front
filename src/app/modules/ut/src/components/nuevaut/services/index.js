d82d9d5310a53cb62b74a14368f17e7460d5401e
Adriano Vega Llobell <adrianovega@hotmail.com>
2017-11-12 20:51:29 +0100

    Added getSprintsProductoService

--- src/app/modules/ut/src/components/nuevaut/services/index.js
+++ src/app/modules/ut/src/components/nuevaut/services/index.js
@@ -1,5 +1,6 @@
 export * from './createUT.service';
 export * from './getProductos.service';
 export * from './getProyectos.service';
+export * from './getSprintsProducto.service';
 export * from './getTiposUTProducto.service';
 export * from './getWorkflows.service';

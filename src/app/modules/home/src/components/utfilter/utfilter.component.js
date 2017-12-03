f74b7695fde45d0e31bab16de57ca5d3e8b64ca6
Adriano Vega <adrianovega@hotmail.com>
2017-12-03 11:14:15 +0100

    Añadidos los campos del formulario

--- src/app/modules/home/src/components/utfilter/utfilter.component.js
+++ src/app/modules/home/src/components/utfilter/utfilter.component.js
@@ -3,9 +3,24 @@ import html from './utfilter.component.html';
 import './utfilter.component.css';
 
 @Component({
-  selector: 'tn-ut-list',
+  selector: 'tn-ut-filter',
   template: html,
 })
 export class UTFilterComponent {
   @Input() filterUts;
+  filtro = {
+      IdUt: null,
+      IdAgente: null,
+      IdProducto: null,
+      IdProyecto: null,
+  }
+
+  sprints = [];
+  productos = [];
+  proyectos = [];
+  sprints = [];
+  
+  isIrAEmpty() {
+    return this.filtro.IdUt === null || this.filtro.IdUt.length === 0;
+  }
 }
